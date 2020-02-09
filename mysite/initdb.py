2ab9e984c4e289822bd79501b65efab7059fec57
wuchunlong0 <wcl6005@163.com>
2020-02-09 10:03:37 +0800

    demo

--- mysite/initdb.py
+++ mysite/initdb.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     django.setup()
     from django.contrib.auth.models import User, Group, Permission
         
-    user = User.objects.create_superuser('admin', 'admin@test.com','1234qazx')
+    user = User.objects.create_superuser('admin', 'admin@test.com','admin')
     user.save()
     
         
