1e765750eea24f79c73cd662e377668bff5d71ad
wuchunlong0 <wcl6005@163.com>
2021-10-26 22:15:00 +0800

    db

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -3,5 +3,11 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-# Create your tests here.
-print('ok')
+import os
+print(os.system('[ "x$(find ./ -name admin.py)" == "x" ]')) #256
+print(os.system('[ "x$(find ./ -name admin.py1)" == "x" ]')) #0
+print(os.system('[ false ]'))  #0
+print(os.system('[ "x$(find ./ -name static)" == "x" ]')) #256
+print(os.system('[ "x$(find ./ -name static1)" == "x" ]')) #0
+print(os.system('[ "x$(diff forms.py forms2.py)" != "x" ] ')) #256 forms1.py forms2.py相同
+print(os.system('[ "x$(diff forms1.py forms2.py)" != "x" ] ')) #0  forms1.py forms2.py不同
